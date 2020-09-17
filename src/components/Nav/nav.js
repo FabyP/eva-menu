6db3eede2d6bd0dd1d3e3ffa710fa9e8c3614f5b
unknown <mayra.fahrer@iem.thm.de>
2020-09-17 13:26:51 +0200

    Einbau von Kellner Button mit Funktionalität

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import ToggleButton from '../SideDrawer/DrawerToggleButton';
 import {MdShoppingBasket} from 'react-icons/md';
 import { IconContext } from "react-icons";
@@ -7,36 +7,75 @@ import { IconContext } from "react-icons";
 import './nav.css';
 import {useGlobalState} from '../../context/global-context';
 
+import Cookies from 'universal-cookie';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import http from '../../http-common';
 
 import {Link, withRouter} from 'react-router-dom';
 
+    
    
     const NavigationBar = (props) => {
+
         const {cartCount, fetchCartCount} = useGlobalState();
         useEffect(() => {
             // Update the document title using the browser API
             fetchCartCount();
             
           },[]);
+
+        const [error, setError] = useState('');
+
+        const callWaiter = async () => {
+            const cookies = new Cookies();
+            let tableId = cookies.get("table");
+            console.log(tableId);
+            if(tableId == null){
+                setError('Bitte Scannen sie zuerst den QR-Code, damit wir wissen an welchem Tisch sie sich befinden.');
+            } else{
+                setError('');
+               
+                await http.patch('/table/' + tableId, {
+                    waitressCalled: true,
+                })
+                .then(function (response) {
+
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+                
+            }
+
+        }      
         return(
-        <header className="navBar">
-            <nav className="navigation">
-                <div>
-                    <ToggleButton click={props.drawerClickHandler}/>
-                </div>
-                <div className="nav_logo"><a href="/">EVAs Imbiss</a></div>
-                <div className="spacer"></div>
-                <div className="nav_items">
-                    <ul>
-                        <Link to='/cart'>
-                            <IconContext.Provider value={{ color: "white", size: "2rem" }}>
-                                <li className="basket"><span>{cartCount}</span><MdShoppingBasket /></li>
-                            </IconContext.Provider>
-                        </Link>
-                    </ul>
-                </div>
-            </nav>
-        </header>)
+            <header className="navBar">
+                <nav className="navigation">
+                    <div>
+                        <ToggleButton click={props.drawerClickHandler}/>
+                    </div>
+                    <div className="nav_logo"><a href="/">EVAs Imbiss</a></div>
+                    <div className="spacer"></div>
+                    <div>
+                        <button type="button" className="nav-button" onClick={() => callWaiter()}>
+                            Kellner rufen
+                        </button>
+                    </div>
+                    <div className="nav_items">
+                        <ul>
+                            <Link to='/cart'>
+                                <IconContext.Provider value={{ color: "white", size: "2rem" }}>
+                                    <li className="basket"><span>{cartCount}</span><MdShoppingBasket/></li>
+                                </IconContext.Provider>
+                            </Link>
+                        </ul>
+                    </div>
+                </nav>
+                {error ? <div class="alert alert-danger" role="alert">
+                {error}
+                </div> :''}
+            </header>
+        )
     };
 
 /*     return(
